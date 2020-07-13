1fbddabe450e9a277f775fbc99d1811ce036156c
Mink <mink@192.168.2.46>
2020-07-13 16:46:40 +0700

    fix

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const http = require("https"),
-    fs = require("fs");
-// const options = {
-//     key: fs.readFileSync("/etc/letsencrypt/live/screening.trueeye.ai/privkey.pem"),
-//     cert: fs.readFileSync("/etc/letsencrypt/live/screening.trueeye.ai/cert.pem")
-// };
+const http = require("https")
+
 
 app.use(express.static(path.join(__dirname, 'build')));
 
