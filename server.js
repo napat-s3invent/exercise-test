307f02b768687e4581b7d1198d58615090cdbc74
Mink <mink@192.168.2.46>
2020-07-13 16:49:14 +0700

    fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const http = require("https")
+const http = require("http")
 
 
 app.use(express.static(path.join(__dirname, 'build')));
